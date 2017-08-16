2e8f6be2febc20ff9d257bb7f67bfed3d9e8f51c
Cameron <cbross9481@gmail.com>
2017-08-16 09:57:00 -0700

    commiting files in ruby

---
+++ ruby/interior_design.rb
@@ -0,0 +1,10 @@
+application = {
+	:name => "Johnson",
+	email: "testing@example.com",
+	:phone => "9731809999",
+	shade: "dark blue",
+	:wallpaper => "Chevron",
+	Ombre: "So last season"
+}
+
+p application
