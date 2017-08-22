2d752a01007bd6eed052a79e7e5a74555c895520
Cameron <cbross9481@gmail.com>
2017-08-22 10:11:12 -0700

    Modify puppy file

---
+++ ruby/puppy.rb
@@ -0,0 +1,14 @@
+#Species--------
+#Puppies
+
+#Characteristics -------
+#Type: varies
+#Eye color: varies
+#height: varies
+#weight: varies
+#Age: varies
+
+#Behavior ----------
+#Bark
+#Sleepgit p
+#Sprint
