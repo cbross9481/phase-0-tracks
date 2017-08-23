78c9817b7a84b95766c9db501ae5e3072446530b
Cameron <cbross9481@gmail.com>
2017-08-23 16:52:01 -0700

    Modify shout ruby file

---
+++ ruby/shout.rb
@@ -0,0 +1,11 @@
+module Shout
+  def self.yell_angrily(words)
+    words + "!!!" + " :("
+  end
+  def self.yelling_happily(words)
+    words + "!!!" + " :)"
+  end
+end
+
+p Shout.yell_angrily("No Way")
+p Shout.yelling_happily("Yes Way")
