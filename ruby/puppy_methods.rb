4f6de17af46c945b96388194880b9592bd5a4f81
Cameron <cbross9481@gmail.com>
2017-08-24 15:33:07 -0700

    Modify puppy method

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,9 +1,26 @@
 class Puppy
-
   def fetch(toy)
     puts "I brought back the #{toy}!"
     toy
   end
 
-end
+  def speak(x)
+  	x.times do 
+  		puts "Woof"
+    end 
+  end 
+  
+  def roll_over
+    puts "rolls over"
+  end 
+  
+  def dog_year(x)
+    puts x * 7
+  end 
+end 
 
+spot = Puppy.new
+spot.fetch("bone")
+spot.speak(3)
+spot.roll_over
+spot.dog_year(5)
